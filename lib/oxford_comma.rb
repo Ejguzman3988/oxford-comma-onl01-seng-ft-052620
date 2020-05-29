9334204e6725deebdec901d42f4ca3e905800b5a
Eriberto Guzman <eriberto176@gmail.com>
2020-05-29 21:29:41 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,4 +1,12 @@
 def oxford_comma(array)
   
+  newString = ""
+  
+  array.collect.each_with_index{|string, index| 
+    if index == array.length-1
+      newString << ", and #{string}."
+    else
+      newString << ", #{string}"
+  }
   
 end
